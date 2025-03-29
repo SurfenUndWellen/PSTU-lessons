419360dd16b88dc1a2105f469a0751f28f28c46c
SurfenUndWellen <mkola9737@gmail.com>
2025-03-29 23:28:01 +0300

    init

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -32,4 +36,6 @@ def login():
     return render_template('login.html')
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
