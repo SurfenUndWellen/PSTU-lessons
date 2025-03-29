a4c0f7c348dacdfaa7ff3fbdba9653fa03bb42e5
SurfenUndWellen <mkola9737@gmail.com>
2025-03-30 00:22:04 +0300

    init

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
-class User(db.Model):
-    id = db.column(db.Integer, primary_key=True)
-    login = db.column(db.string(80), unique=True, nullable=False)
-    password = db.column(db.string(80), unique=True, nullable=False)
-    group = db.Column(db.String(25), nullable=False)
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=True, nullable=False)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 @app.route('/')
+@login_required
 def home():
     return render_template('index.html')
 @app.route('/kg')
@@ -31,8 +42,17 @@ def el():
 @app.route('/student')
 def student():
     return render_template('student.html')
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for('home'))
+        else:
+            flash('Invalid username or password')
     return render_template('login.html')
 with app.app_context():
     db.create_all()
